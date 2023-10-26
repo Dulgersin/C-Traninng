161cace2fcaa71fbb67439c8161ef04b84bf71a7
VladDulger <diversionerios@gmail.com>
2023-10-26 11:00:08 +0600

    Начало работы с entity (code-first)

---
+++ CRM2/CrmBL/Models/CrmContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrmBL.Models
+{
+    public class CrmContext : DbContext
+    {
+        public CrmContext()
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<Check> Checks { get; set; }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Sell> Sells { get; set; }
+
+        public DbSet<Seller> Sellers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=CRM2;Username=postgres;Password=4514");
+        }
+    }
+}
